16b012bc8d11fec6ccd0f4c6fc042965b1a26e4c
Seth Geyer <seth.geyer@gmail.com>
2014-06-23 09:42:25 -0600

    code to ssolve the largest number

--- numbers.rb
+++ numbers.rb
@@ -1,3 +1,14 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
 
+# Count Elements
 puts "Numbers in the numbers array: #{numbers.count}"
+
+
+# Find Largest Number
+sorted_array = numbers.sort
+puts "Largest number in the aray:  #{sorted_array[-1]}"
+
+
+# Sum the values in the numbers array
+total = numbers.inject(0) { |sum, number| sum + number}
+p total
